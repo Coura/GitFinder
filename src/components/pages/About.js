1dc0b90dd019ab9536e8256f85f9cf83a053bbeb
Rui Guerreiro <you@example.com>
2020-06-30 18:17:58 +0200

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className='all-center my-3'>
             <h1>About this Web App</h1>
             <p>App destined to search and find GitHub Users.</p>
-            <p>This app was developed as an exercise for a Udemy Course "React Front To Back". </p>
+            <p>This app was developed by <a href="https://ruiguerreiro.herokuapp.com/">Rui Guerreiro</a> as an exercise for a Udemy Course "React Front To Back". </p>
             <p>Version 1.0.0</p>
         </div>
     )
