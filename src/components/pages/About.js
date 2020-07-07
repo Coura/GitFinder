d512ac233f0773d93642409063c80de1b7a8ea97
Rui Guerreiro <you@example.com>
2020-07-07 15:24:47 +0200

    gitabout

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,8 @@ const About = () => {
             <h1>About this Web App</h1>
             <p>App destined to search and find GitHub Users.</p>
             <p>This app was developed by <a href="https://ruiguerreiro.herokuapp.com/">Rui Guerreiro</a> as an exercise for a Udemy Course "React Front To Back". </p>
-            <p>Version 1.0.0</p>
+            <p>This app uses the React Context API</p>
+            <p>Version 1.0.1</p>
         </div>
     )
 }
